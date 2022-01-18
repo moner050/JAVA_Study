d698769e10a707957526217e4a04c1dc1dbf3c10
moner050 <83222976+moner050@users.noreply.github.com>
2022-01-18 23:23:06 +0900

    JAVA 알고리즘 미로찾기 문제 학습

---
+++ Java_Programing/Algorithm/src/ch06/move/Move.java
@@ -0,0 +1,17 @@
+package ch06.move;
+
+public class Move {
+
+	public int direction=0;
+	
+	public int x=0;
+	public int y=0;
+	
+	public Move(int x,int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	
+}
