4f05f1b7de7cb5b143696fa9b7c1b405208b2239
moner050 <83222976+moner050@users.noreply.github.com>
2022-01-25 23:23:22 +0900

    JAVA 디버그 테스트

---
+++ debug_Test/src/main/java/org/example/util/Calculator.java
@@ -0,0 +1,10 @@
+package org.example.util;
+
+public class Calculator {
+
+    // 디버그 테스트를 위한 int 를 반환하는 static 메서드 add
+    public static int add(int a, int b)
+    {
+        return a + b;
+    }
+}
