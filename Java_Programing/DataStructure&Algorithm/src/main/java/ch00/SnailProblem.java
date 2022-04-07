4ba158c34c3321bd5d2a4cf62fe0e96f60c21976
moner050 <minhyung0237@gmail.com>
2022-04-07 19:47:18 +0900

    fix: Change Encoding UTF-8
    Encoding을 UTF-8로 바꿈

--- Java_Programing/DataStructure&Algorithm/src/main/java/ch00/SnailProblem.java
+++ Java_Programing/DataStructure&Algorithm/src/main/java/ch00/SnailProblem.java
@@ -1,2 +1,28 @@
-package ch00;public class SnailProblem {
+package ch00;
+
+public class SnailProblem {
+
+    public static void main(String[] args) {
+
+        // 달팽이가 하루에 3M를 올라가지만 잠잘때는 2M가 미끄러져 내려간다.
+        // 달팽이가 10M 나무를 올라가는데 걸리는 일수를 구하시오.
+
+        int day = 0, currentHeight = 0, treeHeight = 10, upHeight = 3, downHeight = 2;
+
+        while(true)
+        {
+            day++;
+
+            currentHeight += upHeight;
+
+            if(currentHeight >= treeHeight) break;
+
+            currentHeight -= downHeight;
+        }
+
+        System.out.println(day + "일 걸렸습니다.");
+
+
+    }
+
 }
