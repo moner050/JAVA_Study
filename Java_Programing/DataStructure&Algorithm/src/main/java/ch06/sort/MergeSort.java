b783fb84ef873fdd4349abec6d77f6ad7bd9a53b
moner050 <minhyung0237@gmail.com>
2022-04-11 16:24:26 +0900

    feat: Create MergeSort

---
+++ Java_Programing/DataStructure&Algorithm/src/main/java/ch06/sort/MergeSort.java
@@ -0,0 +1,70 @@
+package ch06.sort;
+
+public class MergeSort implements ISort{
+    @Override
+    public void sort(int[] arr) {
+        // in-place sort
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    // 분할
+    private void mergeSort(int[] arr, int low, int high)
+    {
+        if(low >= high)
+        {
+
+        }
+
+        int mid = low + ((high - low) / 2);
+        mergeSort(arr, low, mid);
+        mergeSort(arr,mid + 1, high);
+
+        merge(arr, low, mid, high);
+    }
+
+
+
+    // 합병
+    private void merge(int[] arr, int low, int mid, int high)
+    {
+        int temp[] = new int[high - low + 1];
+        int idx = 0;
+
+        // 분할된 왼쪽, 오른쪽 인덱스의 시작 인덱스
+        int left = low;
+        int right = mid + 1;
+        while(left <= mid && right <= high)
+        {
+            if(arr[left] <= arr[right])
+            {
+                temp[idx] = arr[left];
+                left++;
+            }
+            else
+            {
+                temp[idx] = arr[right];
+                right++;
+            }
+            idx++;
+        }
+        // 왼쪽에 값이 남아있는 경우
+        while(left <= mid)
+        {
+            temp[idx] = arr[left];
+            idx++;
+            left++;
+        }
+        // 오른쪽에 값이 남아있는 경우
+        while(right <= high)
+        {
+            temp[idx] = arr[right];
+            idx++;
+            right++;
+        }
+
+        for(int i = low; i <= high; i++)
+        {
+            arr[i] = temp[i - low];
+        }
+    }
+}
