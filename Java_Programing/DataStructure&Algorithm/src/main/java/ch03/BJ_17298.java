0c6bf80efb5c2b0e36709b1b9db6067eb7bb9c42
moner050 <minhyung0237@gmail.com>
2022-04-28 02:23:57 +0900

    docs: Add annotation
    작동방식 주석 추가

--- Java_Programing/DataStructure&Algorithm/src/main/java/ch03/BJ_17298.java
+++ Java_Programing/DataStructure&Algorithm/src/main/java/ch03/BJ_17298.java
@@ -23,21 +23,21 @@ public class BJ_17298 {
 
         for(int i = 0; i < A; i++)
         {
-            // 스택이 비어있지 않고 현재 num 의 숫자의 값이 num[stack.peek] 보다 클때까지
+            // 스택이 비어있지 않고 현재 num 의 숫자의 값이 num[stack.peek] 보다 크기 전까지
             while(!stack.isEmpty() && (num[i] > num[stack.peek()]) )
-            {   // num 의 stack.pop
+            {   // num[stack.pop] 에 num[i] 넣어주기 (큰 수 넣어주기)
                 num[stack.pop()] = num[i];
             }
-            // 스택에 길이만큼의 인덱스를 push 해주기
+            // 스택에 길이만큼의 인덱스를 push 해주기 (자연스럽게 마지막 인덱스 넣어줌.)
             stack.push(i);
         }
 
-
+        // 그다음 스택에 남아있는 인덱스값을 이용해 num 에 -1 넣어주기
         while(!stack.isEmpty())
         {
             num[stack.pop()] = -1;
         }
-
+        // sb 에 추가 후 출력해주기
         for(int i = 0; i < A; i++)
         {
             sb.append(num[i] + " ");
