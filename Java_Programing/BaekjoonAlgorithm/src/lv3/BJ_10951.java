65c3f15459d0a00240db62d75e3c100b463e1339
moner050 <minhyung0237@gmail.com>
2022-05-01 03:13:21 +0900

    feat: create BJ lv3 questions
    백준 lv3 문제 풀어보기

---
+++ Java_Programing/BaekjoonAlgorithm/src/lv3/BJ_10951.java
@@ -0,0 +1,24 @@
+package lv3;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BJ_10951 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        String temp;
+
+        while( (temp = br.readLine()) != null )
+        {
+            StringTokenizer st = new StringTokenizer(temp, " ");
+
+            int fst = Integer.parseInt(st.nextToken());
+            int scd = Integer.parseInt(st.nextToken());
+            sb.append(fst+scd + "\n");
+        }
+        bw.write(sb + "");
+        bw.close();
+    }
+}
