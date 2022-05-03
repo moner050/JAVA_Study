7eb23a191569758e67b1df715a2037283e10aaad
moner050 <minhyung0237@gmail.com>
2022-05-03 23:55:10 +0900

    feat: add Validity check to register, insert, delete
    회원삽입, 수정, 삭제에 같은 아이디가 있나 유효성 체크 기능 추가

--- JavaProgrammingToyProject/src/member/MemberManager.java
+++ JavaProgrammingToyProject/src/member/MemberManager.java
@@ -10,6 +10,7 @@ public class MemberManager {
 
 	BufferedReader br = null;
 	MemberDao memberDao = null;
+	MemberRegister memberRegister = new MemberRegister();
 	
 	String menuList = "목록을 원하시면 1번을 입력하세요.\r\n"
 					+ "등록을 원하시면 2번을 입력하세요.\r\n"
@@ -30,46 +31,46 @@ public class MemberManager {
 			System.out.println(menuList);
 			int n = Integer.parseInt(br.readLine());
 			if(n == 0) break;
-			else if(n == 1) 
-			{
-				memberDao.getMemberList();
-			}
+			else if(n == 1) memberDao.getMemberList();
 			else if(n == 2)
 			{
 				System.out.print("아이디를 입력하세요 (형식 M-00001): ");
 				String member_id = br.readLine();
-				System.out.print("이름을 입력하세요 : ");
-				String name = br.readLine();
-				System.out.print("전화번호를 입력하세요 : ");
-				String phone_number = br.readLine();
-				if(memberDao.insertMember(member_id, name, phone_number))
-				{
-					System.out.println("---> 회원가입에 성공하셨습니다.");
-				}
-				else System.out.println("---> 회원가입에 실패하셨습니다.");
+				memberRegister.registerCheck(member_id);
 			}
 			else if(n == 3)
 			{
 				System.out.print("수정할 아이디를 입력하세요 (형식 M-00001): ");
 				String member_id = br.readLine();
-				System.out.print("수정할 전화번호를 입력하세요 : ");
-				String phone_number = br.readLine();
-				if(memberDao.updateMember(phone_number, member_id))
+				if(!memberDao.equalMember(member_id))
 				{
-					System.out.println("---> 회원수정에 성공하셨습니다.");
+					System.out.println("일치하는 아이디가 없습니다.");
 				}
-				else System.out.println("---> 회원수정에 실패하셨습니다.");
+				else
+				{
+					System.out.print("수정할 전화번호를 입력하세요 : ");
+					String phone_number = br.readLine();
+					if(memberDao.updateMember(phone_number, member_id))
+					{
+						System.out.println("---> 회원수정에 성공하셨습니다.");
+					}
+					else System.out.println("---> 회원수정에 실패하셨습니다.");
+				}
 			}
 			else if(n == 4)
 			{
 				System.out.print("삭제할 아이디를 입력하세요 (형식 M-00001): ");
 				String member_id = br.readLine();
-				if(memberDao.deleteMember(member_id))
+				if(memberDao.equalMember(member_id))
 				{
-					System.out.println("---> " + member_id + "회원 삭제에 성공하셨습니다.");
+					if(memberDao.deleteMember(member_id))
+					{
+						System.out.println("---> " + member_id + "회원 삭제에 성공하셨습니다.");
+					}
+					else System.out.println("잘못 입력하셨습니다.");
 				}
+				else System.out.println("삭제할" + member_id + " 회원 정보가 존재하지 않습니다.");
 			}
-			else System.out.println("잘못 입력하셨습니다.");
 		}
 	}
 
