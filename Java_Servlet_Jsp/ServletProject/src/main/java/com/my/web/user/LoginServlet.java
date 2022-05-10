4baa8654f779614bce36a44a581f85d405a4f160
moner050 <minhyung0237@gmail.com>
2022-05-10 22:13:57 +0900

    feat: Create LoginServlet
    로그인 인증 처리를 위한 로그인서블렛 생성

---
+++ Java_Servlet_Jsp/ServletProject/src/main/java/com/my/web/user/LoginServlet.java
@@ -0,0 +1,23 @@
+package com.my.web.user;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("로그인 인증 처리 로직 작성");
+	}
+
+}
