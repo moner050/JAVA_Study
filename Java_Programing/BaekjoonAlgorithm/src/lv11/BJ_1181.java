da50c3e8184440d86a91dfdf24a5c617a9eb1d7f
moner050 <minhyung0237@gmail.com>
2022-06-23 14:19:27 +0900

    feat: Create BJ_1181
    백준 lv11 1181번 문제 풀어보기

---
+++ Java_Programing/BaekjoonAlgorithm/src/lv11/BJ_1181.java
@@ -0,0 +1,45 @@
+package lv11;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BJ_1181 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 몇개의 단어를 넣을지 설정
+        int N = Integer.parseInt(br.readLine());
+        String[] text = new String[N];
+
+        // 문자열 입력받아 배열에 넣어주기
+        for (int i = 0; i < N; i++)
+        {
+            text[i] = br.readLine();
+        }
+
+        // 문자열 중복 제거
+        String[] distinctText = Arrays.stream(text).distinct().toArray(String[]::new);
+        // 문자열 오름차순 및 사전순 정렬
+        Arrays.sort(distinctText, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length()) return o1.compareTo(o2);
+                else return o1.length() - o2.length();
+            }
+        });
+
+        // 결과값 출력
+        for (String s : distinctText){
+            bw.write(s + "\n");
+        }
+        bw.close();
+    }
+
+}
