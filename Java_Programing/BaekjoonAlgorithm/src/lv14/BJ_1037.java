eb69767faf08a6641667441996d5f4f9fede82aa
moner050 <minhyung0237@gmail.com>
2022-08-15 22:24:06 +0900

    feat: Create BJ_1037
    백준 lv14 1037번 문제 풀어보기

---
+++ Java_Programing/BaekjoonAlgorithm/src/lv14/BJ_1037.java
@@ -0,0 +1,29 @@
+package lv14;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_1037 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int min = arr[0];
+        int max = arr[arr.length-1];
+
+        System.out.println(min * max);
+    }
+}
