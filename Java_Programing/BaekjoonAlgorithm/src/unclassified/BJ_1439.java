da42575d35e5f1d9c1d012d9665405c269bf551d
moner050 <minhyung0237@gmail.com>
2022-08-18 21:10:12 +0900

    feat: Create BJ_1439
    백준 1439번 문제 풀어보기

---
+++ Java_Programing/BaekjoonAlgorithm/src/unclassified/BJ_1439.java
@@ -0,0 +1,20 @@
+package unclassified;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_1439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = br.readLine();
+        String[] temp = S.split("");
+
+        StringTokenizer zero = new StringTokenizer(S, "0");
+        StringTokenizer one = new StringTokenizer(S, "1");
+
+        System.out.println(Math.min(zero.countTokens(), one.countTokens()));
+    }
+}
