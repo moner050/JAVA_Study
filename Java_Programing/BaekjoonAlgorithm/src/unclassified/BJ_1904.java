8efec4a1792018e5e040e8f79451e78f5cc7ad64
moner050 <minhyung0237@gmail.com>
2022-08-21 02:22:47 +0900

    feat: Create BJ_1904
    백준 1904번 문제 풀어보기

---
+++ Java_Programing/BaekjoonAlgorithm/src/unclassified/BJ_1904.java
@@ -0,0 +1,26 @@
+package unclassified;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_1904 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long[] arr = new long[1000000 + 1];
+        
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 3;
+        arr[4] = 5;
+
+        for (int i = 4; i <= 1000000; i++) {
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 15746;
+        }
+
+        System.out.println(arr[N]);
+    }
+}
