5bfa123765504927df09ebf643ad26e71aa4f00a
moner050 <minhyung0237@gmail.com>
2022-09-24 14:09:42 +0900

    feat: Create BJ_2133
    백준 2133번 문제 풀어보기

---
+++ Java_Programing/BaekjoonAlgorithm/src/unclassified/BJ_2133.java
@@ -0,0 +1,28 @@
+package unclassified;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_2133 {
+    static int[] d = new int[1001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(dp(Integer.parseInt(br.readLine())));
+    }
+    public static int dp(int x){
+        if(x == 0) return 1;
+        if(x == 1) return 0;
+        if(x == 2) return 3;
+        if(d[x] != 0) return d[x];
+
+        int result = 3 * dp(x - 2);
+
+        for (int i = 3; i <= x; i++) {
+            if(i % 2 == 0){
+                result += 2 * dp(x - i);
+            }
+        }
+        return d[x] = result;
+    }
+}
