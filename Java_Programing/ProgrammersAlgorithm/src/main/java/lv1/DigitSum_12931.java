c98f9661f6bba0c11da5dac77e7af1218e0f0b13
moner050 <minhyung0237@gmail.com>
2022-09-24 13:48:07 +0900

    feat: Create DigitSum_12931
    자릿수 더하기 문제 풀어보기

---
+++ Java_Programing/ProgrammersAlgorithm/src/main/java/lv1/DigitSum_12931.java
@@ -0,0 +1,19 @@
+package lv1;
+
+import java.util.Arrays;
+
+public class DigitSum_12931 {
+    public static void main(String[] args) {
+        System.out.println(solution(987));
+    }
+
+    public static int solution(int n){
+        int answer = 0;
+        String[] temp = String.valueOf(n).split("");
+        int[] intTemp = Arrays.stream(temp).mapToInt(Integer::parseInt).toArray();
+        for(int num : intTemp){
+            answer += num;
+        }
+        return answer;
+    }
+}
