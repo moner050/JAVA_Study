59313d5bb39b388f11fce3ac53be07700e6f9378
moner050 <minhyung0237@gmail.com>
2022-09-25 18:01:52 +0900

    feat: Create Square_12934
    정수 제곱근 판별 문제 풀어보기

---
+++ Java_Programing/ProgrammersAlgorithm/src/main/java/lv1/Square_12934.java
@@ -0,0 +1,17 @@
+package lv1;
+
+public class Square_12934 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(121));
+        System.out.println(solution(3));
+    }
+
+    public static long solution(long n) {
+        double sqrt = Math.pow(n, 0.5);
+        if(sqrt % 1 == 0){
+            return (long) Math.pow((sqrt + 1), 2);
+        }
+        else return -1;
+    }
+}
