c8b7cf2802fe0b02e07d837c2046c0e7ff77c3b9
moner050 <minhyung0237@gmail.com>
2022-10-27 17:48:16 +0900

    Feat: Create SumOfDivisor_12928

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv1/SumOfDivisor_12928.java
@@ -0,0 +1,29 @@
+package programmers.lv1;
+
+public class SumOfDivisor_12928 {
+    public static void main(String[] args) {
+        System.out.println(solution1(12));
+        System.out.println(solution2(12));
+    }
+
+    public static int solution1(int num) {
+        int answer = 0;
+        for (int i = 1; i <= num ; i++) {
+            if(num % i == 0) answer += i;
+        }
+        return answer;
+    }
+
+    public static int solution2(int num) {
+        int answer = 0;
+        for (int i = 1; i <= Math.sqrt(num) ; i++) {
+            if(num % i == 0) {
+                answer += i;
+                if(num / i != i) {
+                    answer += (num / i);
+                }
+            }
+        }
+        return answer;
+    }
+}
