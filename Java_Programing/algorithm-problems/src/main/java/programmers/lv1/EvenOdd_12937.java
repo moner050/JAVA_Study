f849afccc91e5f331235dbbad2d8eb84ad0ecd0a
moner050 <minhyung0237@gmail.com>
2022-10-27 17:46:07 +0900

    Feat: Create EvenOdd_12937

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv1/EvenOdd_12937.java
@@ -0,0 +1,13 @@
+package programmers.lv1;
+
+public class EvenOdd_12937 {
+
+    public static void main(String[] args) {
+
+        System.out.println(solution(5));
+    }
+    public static String solution(int num) {
+        String answer = (num % 2 == 0) ? "Even" : "Odd";
+        return answer;
+    }
+}
