ca9e1b8aea73b662d1d33360b82d76aa9051fe3f
moner050 <minhyung0237@gmail.com>
2022-10-27 17:44:30 +0900

    Feat: Create Average_12944

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv1/Average_12944.java
@@ -0,0 +1,17 @@
+package programmers.lv1;
+
+public class Average_12944 {
+    public static void main(String[] args) {
+        int[] temp = {1,2,3,4};
+        System.out.println(solution(temp));
+    }
+
+    public static double solution(int[] arr) {
+        double answer = 0;
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        answer /= arr.length;
+        return answer;
+    }
+}
