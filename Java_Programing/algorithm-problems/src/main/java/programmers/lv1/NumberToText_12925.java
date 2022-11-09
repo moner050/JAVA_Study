7543dbf573cb4d753fc3ba6f0757139cdb03883c
moner050 <minhyung0237@gmail.com>
2022-11-09 21:50:17 +0900

    Feat: Create NumberToText_12925

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv1/NumberToText_12925.java
@@ -0,0 +1,13 @@
+package programmers.lv1;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12925
+public class NumberToText_12925 {
+    public static void main(String[] args) {
+        String str = "-1234";
+        System.out.println(solution(str));
+    }
+
+    public static int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
