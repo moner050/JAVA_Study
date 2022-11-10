352652cda314f4593924af43e46fe57311f5a92c
moner050 <minhyung0237@gmail.com>
2022-11-10 19:04:34 +0900

    Feat: Modify HashadNum_12947

--- Java_Programing/algorithm-problems/src/main/java/programmers/lv1/HashadNum_12947.java
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv1/HashadNum_12947.java
@@ -3,19 +3,26 @@ package programmers.lv1;
 // https://school.programmers.co.kr/learn/courses/30/lessons/12947
 public class HashadNum_12947 {
     public static void main(String[] args) {
-//        int arr = 12;
-        int arr = 10;
+        int arr = 12;
+//        int arr = 10;
 
-        System.out.println(solution(arr));
+        System.out.println(solution1(arr));
+        System.out.println(solution2(arr));
     }
 
-    public static boolean solution(int x) {
+    public static boolean solution1(int x) {
         int sum = 0;
         String[] numList = String.valueOf(x).split("");
 
         for (String num : numList) {
             sum += Integer.parseInt(num);
         }
+
+        return x % sum == 0;
+    }
+
+    public static boolean solution2(int x) {
+        int sum = String.valueOf(x).chars().map(n -> n - '0').sum();
 
         return x % sum == 0;
     }
