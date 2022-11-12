90261fcffe5e9bb844ec97c5d41040d69fa9148a
moner050 <minhyung0237@gmail.com>
2022-11-12 21:57:06 +0900

    Feat: Modify BetweenSum_12912

--- Java_Programing/algorithm-problems/src/main/java/programmers/lv1/BetweenSum_12912.java
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv1/BetweenSum_12912.java
@@ -5,9 +5,11 @@ public class BetweenSum_12912 {
     public static void main(String[] args) {
         int a = 3;
         int b = 5;
-        System.out.println(solution(a, b));
+        System.out.println(solution1(a, b));
+        System.out.println(solution2(a, b));
     }
-    public static long solution(int a, int b) {
+
+    public static long solution1(int a, int b) {
         long answer = 0;
         int min = Math.min(a, b);
         int max = Math.max(a, b);
@@ -16,5 +18,12 @@ public class BetweenSum_12912 {
             answer += i;
         }
         return answer;
+    }
+
+    public static long solution2(int a, int b) {
+        long min = Math.min(a, b);
+        long max = Math.max(a, b);
+
+        return (max - min + 1) * (min + max) / 2;
     }
 }
