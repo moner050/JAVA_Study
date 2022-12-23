0524a17303a95eec9d9b84a35ff7335383369a64
moner050 <minhyung0237@gmail.com>
2022-12-23 12:14:28 +0900

    Feat: Create PlusResult_120802
    두 수의 합 문제 풀어보기

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv0/PlusResult_120802.java
@@ -0,0 +1,15 @@
+package programmers.lv0;
+
+// 두 수의 합
+// https://school.programmers.co.kr/learn/courses/30/lessons/120802
+public class PlusResult_120802 {
+    public static void main(String[] args) {
+        int num1 = 2, num2 = 3;
+        System.out.println(solution(num1, num2));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
