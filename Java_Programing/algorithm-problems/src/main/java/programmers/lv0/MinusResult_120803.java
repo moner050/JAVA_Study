5b5b95ba5d770edf61037c6dd584b73a747adb82
moner050 <minhyung0237@gmail.com>
2022-12-23 11:50:21 +0900

    Feat: Create MinusResult_120803
    두수의 차 문제 풀어보기

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv0/MinusResult_120803.java
@@ -0,0 +1,15 @@
+package programmers.lv0;
+
+// 두수의 차
+// https://school.programmers.co.kr/learn/courses/30/lessons/120803
+public class MinusResult_120803 {
+    public static void main(String[] args) {
+        int num1 = 2, num2 = 3;
+        System.out.println(solution(num1, num2));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+}
+
