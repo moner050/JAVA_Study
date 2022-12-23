ddcc752ade868e252df9e3cde56c35879347238d
moner050 <minhyung0237@gmail.com>
2022-12-23 12:34:17 +0900

    Feat: Create Division_120806
    두 수의 나눗셈 문제 풀어보기

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv0/Division_120806.java
@@ -0,0 +1,15 @@
+package programmers.lv0;
+
+// 두 수의 나눗셈
+// https://school.programmers.co.kr/learn/courses/30/lessons/120806
+public class Division_120806 {
+    public static void main(String[] args) {
+        int num1 = 3, num2 = 2;
+        System.out.println(solution(num1, num2));
+    }
+
+    public static int solution(int num1, int num2) {
+        return (int)(((double) num1 / num2) * 1000);
+    }
+}
+
