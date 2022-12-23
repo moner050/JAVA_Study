62e52cccf539ee76d57c2d885f1eb83e83ef7d33
moner050 <minhyung0237@gmail.com>
2022-12-23 12:06:20 +0900

    Feat: Create Division_120805
    몫 구하기 문제 풀어보기

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv0/Division_120805.java
@@ -0,0 +1,15 @@
+package programmers.lv0;
+
+// 몫 구하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/120805
+public class Division_120805 {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 5;
+        System.out.println(solution(num1, num2));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+}
+
