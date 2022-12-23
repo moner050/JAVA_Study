520cd6a0a8935161b2d1891b942aed9fc0697a29
moner050 <minhyung0237@gmail.com>
2022-12-23 11:59:41 +0900

    Feat: Create Remainder_120810
    나머지 구하기 문제 풀어보기

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv0/Remainder_120810.java
@@ -0,0 +1,15 @@
+package programmers.lv0;
+
+// 나머지 구하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/120810
+public class Remainder_120810 {
+    public static void main(String[] args) {
+        int num1 = 3, num2 = 2;
+        System.out.println(solution(num1, num2));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 % num2;
+    }
+}
+
