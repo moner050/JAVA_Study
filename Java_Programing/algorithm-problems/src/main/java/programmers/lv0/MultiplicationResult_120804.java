26aec342f432d7d19d618e3d935763f7d17b315d
moner050 <minhyung0237@gmail.com>
2022-12-23 11:52:31 +0900

    Feat: Create MultiplicationResult_120804
    두 수의 곱 문제 풀어보기

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv0/MultiplicationResult_120804.java
@@ -0,0 +1,13 @@
+package programmers.lv0;
+
+// 두 수의 곱
+// https://school.programmers.co.kr/learn/courses/30/lessons/120804
+public class MultiplicationResult_120804 {
+    public static void main(String[] args) {
+        int num1 = 3, num2 = 4;
+        System.out.println(solution(num1, num2));
+    }
+    public static int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+}
