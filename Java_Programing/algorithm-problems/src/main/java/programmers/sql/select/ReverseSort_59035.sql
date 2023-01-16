8af914b12bceb96fd78693fa5af3d16d722dd47c
moner050 <minhyung0237@gmail.com>
2023-01-16 19:36:18 +0900

    Feat: Create ReverseSort_59035.sql
    역순 정렬하기 문제 풀어보기

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/sql/select/ReverseSort_59035.sql
@@ -0,0 +1,27 @@
+-- 역순 정렬하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59035
+
+--- DDL
+CREATE TABLE ANIMAL_INS (
+    ANIMAL_ID VARCHAR(255) PRIMARY KEY NOT NULL,
+    ANIMAL_TYPE VARCHAR(255) NOT NULL,
+    DATETIME DATETIME NOT NULL,
+    INTAKE_CONDITION VARCHAR(255) NOT NULL,
+    NAME VARCHAR(255),
+    SEX_UPON_INTAKE VARCHAR(255) NOT NULL
+);
+---
+
+--- DML(INSERT)
+INSERT INTO ANIMAL_INS(ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE)
+VALUES('A349996', '	Cat', '2018-01-22 14:32:00', 'Normal', 'Sugar', 'Neutered Male'),
+      ('A350276', '	Cat', '2017-08-13 13:50:00', 'Normal', 'Jewel', 'Spayed Female'),
+      ('A350375', '	Cat', '2017-03-06 15:01:00', 'Normal', 'Meo', 'Neutered Male'),
+      ('A352555', '	Dog', '2014-08-08 04:20:00', 'Normal', 'Harley', 'Spayed Female');
+---
+
+--- DML(SELECT)
+SELECT NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID DESC;
+---
