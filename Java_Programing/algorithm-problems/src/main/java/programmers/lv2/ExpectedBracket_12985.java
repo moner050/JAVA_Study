ceec4e810982d75bc6488d97c2abff8b356c00cf
moner050 <minhyung0237@gmail.com>
2023-01-18 17:45:15 +0900

    Feat: Create ExpectedBracket_12985
    예상 대진표 문제 풀어보기

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv2/ExpectedBracket_12985.java
@@ -0,0 +1,31 @@
+package programmers.lv2;
+
+// 예상 대진표
+// https://school.programmers.co.kr/learn/courses/30/lessons/12985
+public class ExpectedBracket_12985 {
+    public static void main(String[] args) {
+        int n = 8, a = 4, b = 7;
+
+        System.out.println(solution(n, a, b));
+    }
+
+    public static int solution(int n, int a, int b) {
+        int cnt = 1;
+
+        while(Math.abs(a - b) != 1 || a / 2 == b / 2) {
+            a = nextNumber(a);
+            b = nextNumber(b);
+            cnt++;
+        }
+
+        return cnt;
+    }
+
+    public static int nextNumber(int num) {
+        if(num % 2 == 1) num = num / 2 + 1;
+        else num /= 2;
+
+        return num;
+    }
+
+}
