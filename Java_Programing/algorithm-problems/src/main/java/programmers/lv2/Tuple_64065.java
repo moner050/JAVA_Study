8b5eb6fe4adffbc68e72cc89e0e0bb55ee0bedd3
moner050 <minhyung0237@gmail.com>
2023-02-01 21:48:11 +0900

    Feat: Create Tuple_64065
    튜플 문제 풀어보기

--- Java_Programing/algorithm-problems/src/main/java/programmers/lv2/Tuple_64065.java
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv2/Tuple_64065.java
@@ -2,6 +2,7 @@ package programmers.lv2;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 // 튜플
 // https://school.programmers.co.kr/learn/courses/30/lessons/64065
@@ -11,10 +12,11 @@ public class Tuple_64065 {
 //        String s = "{{123}}";
         String s = "{{4,2,3},{3},{2,3,4,1},{2,3}}";
 
-        System.out.println(Arrays.toString(solution(s)));
+        System.out.println(Arrays.toString(solution1(s)));
+        System.out.println(Arrays.toString(solution2(s)));
     }
 
-    public static int[] solution(String s) {
+    public static int[] solution1(String s) {
         int[] result = {};
         String[] arr = s.split("\\},\\{");
 
@@ -46,5 +48,23 @@ public class Tuple_64065 {
         }
 
         return answer;
+    }
+
+    public static int[] solution2(String s) {
+        int idx = 0;
+        Set<String> set = new HashSet<>();
+        // { 와 } 를 띄어쓰기로 바꿔준 뒤 문자 맨 왼쪽, 오른쪽 공백을 제거 후 문자 사이에 , 를 기준으로 배열생성.
+        String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
+        // 배열 크기 오름차순으로 정렬
+        Arrays.sort(arr, (x, y) -> {return x.length() - y.length();});
+
+        int[] result = new int[arr.length];
+        for (String s1 : arr) {
+            for (String s2 : s1.split(",")) {
+                if(set.add(s2)) result[idx++] = Integer.parseInt(s2);
+            }
+        }
+
+        return result;
     }
 }
