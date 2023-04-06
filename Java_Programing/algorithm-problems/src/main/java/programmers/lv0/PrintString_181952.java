347081d1c9fd966f0b762858dedcfc63756e2b00
moner050 <minhyung0237@gmail.com>
2023-04-06 22:06:47 +0900

    Feat: Create PrintString_181952
    문자열 출력하기 문제 풀어보기

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv0/PrintString_181952.java
@@ -0,0 +1,14 @@
+package programmers.lv0;
+
+import java.util.Scanner;
+
+// 문자열 출력하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181952
+public class PrintString_181952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a);
+    }
+}
