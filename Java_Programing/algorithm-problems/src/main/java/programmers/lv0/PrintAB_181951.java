b2dbc0345c24238878d177b6eb4d9f398609bdfe
moner050 <minhyung0237@gmail.com>
2023-04-06 22:08:03 +0900

    Feat: Create PrintAB_181951
    a와 b 출력하기 문제 풀어보기

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv0/PrintAB_181951.java
@@ -0,0 +1,16 @@
+package programmers.lv0;
+
+import java.util.Scanner;
+
+// a와 b 출력하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181951
+public class PrintAB_181951 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
