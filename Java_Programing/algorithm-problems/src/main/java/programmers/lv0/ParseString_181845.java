06a0c691a9b7530c76e87b7349f7aa234465f349
moner050 <minhyung0237@gmail.com>
2023-04-06 22:20:57 +0900

    Feat: Create ParseString_181845
    문자열로 변환 문제 풀어보기

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv0/ParseString_181845.java
@@ -0,0 +1,15 @@
+package programmers.lv0;
+
+// 문자열로 변환
+// https://school.programmers.co.kr/learn/courses/30/lessons/181845
+public class ParseString_181845 {
+    public static void main(String[] args) {
+        int n = 123;
+
+        System.out.println(solution(n));
+    }
+
+    public static String solution(int n) {
+        return String.valueOf(n);
+    }
+}
