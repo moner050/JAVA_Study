39ef58a95a959347942cb28b12f9d0c1d298488b
moner050 <minhyung0237@gmail.com>
2023-04-09 21:26:09 +0900

    Feat: Create PrintString_181946
    문자열 붙여서 출력하기 문제 풀어보기

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv0/PrintString_181946.java
@@ -0,0 +1,15 @@
+package programmers.lv0;
+
+import java.util.Scanner;
+
+// 문자열 붙여서 출력하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181946
+public class PrintString_181946 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.println(a + b);
+    }
+}
