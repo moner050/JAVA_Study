0356655b6a43c345e5550484c4e47dfb5d32c5a8
moner050 <minhyung0237@gmail.com>
2023-04-14 23:39:58 +0900

    Feat: Create ReturnOtherResultOddEven_181935
    홀짝에 따라 다른 값 반환하기 문제 풀어보기

--- Java_Programing/algorithm-problems/src/main/java/programmers/lv0/ReturnOtherResultOddEven_181935.java
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv0/ReturnOtherResultOddEven_181935.java
@@ -4,12 +4,14 @@ package programmers.lv0;
 // https://school.programmers.co.kr/learn/courses/30/lessons/181935
 public class ReturnOtherResultOddEven_181935 {
     public static void main(String[] args) {
-        int n = 7;
+//        int n = 7;
+        int n = 10;
 
-        System.out.println(solution(n));
+        System.out.println(solution1(n));
+        System.out.println(solution2(n));
     }
 
-    public static int solution(int n) {
+    public static int solution1(int n) {
         int result = 0;
 
         if(n % 2 == 1) {
@@ -24,5 +26,10 @@ public class ReturnOtherResultOddEven_181935 {
         }
 
         return result;
+    }
+
+    public static int solution2(int n) {
+        if (n % 2 == 1) return (n + 1) * (n + 1) / 2 / 2;
+        else return 4 * n/2 * (n/2 + 1) * (2 * n/2 + 1) / 6;
     }
 }
