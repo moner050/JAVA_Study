2a4d3f3d5f4d206d6455c747b7f4fb74d2b0a907
moner050 <minhyung0237@gmail.com>
2023-04-16 22:54:36 +0900

    Feat: Create SeparateBySpace_181868
    공백으로 구분하기 2 문제 풀어보기

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/lv0/SeparateBySpace_181868.java
@@ -0,0 +1,17 @@
+package programmers.lv0;
+
+import java.util.Arrays;
+
+// 공백으로 구분하기 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/181868
+public class SeparateBySpace_181868 {
+    public static void main(String[] args) {
+        String my_string = " i    love  you";
+
+        System.out.println(Arrays.toString(solution(my_string)));
+    }
+
+    public static String[] solution(String my_string) {
+        return my_string.trim().split("\\s+");
+    }
+}
