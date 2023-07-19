4f066c4bf0b5aa147bbd709ceec968b6a1cf71db
moner050 <minhyung0237@gmail.com>
2023-07-19 23:18:43 +0900

    Feat: Create TotalOrderForIceCreamDiviedByIngredients_133026
    성분으로 구분한 아이스크림 총 주문량 문제 풀어보기

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/sql/groupby/TotalOrderForIceCreamDiviedByIngredients_133026.sql
@@ -0,0 +1,9 @@
+-- 성분으로 구분한 아이스크림 총 주문량
+-- https://school.programmers.co.kr/learn/courses/30/lessons/133026
+
+SELECT II.INGREDIENT_TYPE, SUM(FH.TOTAL_ORDER) AS TOTAL_ORDER
+FROM FIRST_HALF FH
+         LEFT JOIN ICECREAM_INFO II
+                   ON FH.FLAVOR = II.FLAVOR
+GROUP BY II.INGREDIENT_TYPE
+ORDER BY TOTAL_ORDER ASC;
