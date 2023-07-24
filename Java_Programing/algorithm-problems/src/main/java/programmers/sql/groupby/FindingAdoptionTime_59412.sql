04e1c03c48ba08fb17f89b55ac14c7cf7673ae57
moner050 <minhyung0237@gmail.com>
2023-07-24 23:07:48 +0900

    Feat: Create FindingAdoptionTime_59412
    입양 시각 구하기(1) 문제 풀어보기

---
+++ Java_Programing/algorithm-problems/src/main/java/programmers/sql/groupby/FindingAdoptionTime_59412.sql
@@ -0,0 +1,8 @@
+-- 입양 시각 구하기(1)
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59412
+
+SELECT HOUR(DATETIME) AS HOUR, COUNT(HOUR(DATETIME)) AS COUNT
+FROM ANIMAL_OUTS
+GROUP BY HOUR
+HAVING HOUR >= 9 AND HOUR < 20
+ORDER BY HOUR ASC;
